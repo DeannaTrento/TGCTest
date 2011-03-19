3567141ff9a3a54a07f90bf7282e4002efdf6505
Deanna Trento <deanna.trento@gmail.com>
2011-03-19 16:49:20 +1100

    Set up OpenAL

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <windows.h>
 #include <GL/freeglut.h>
+#include <AL/alut.h>
+#include <al.h>
+#include <stdio.h>
+
+
+#define NUM_BUFFERS 1
+#define NUM_SOURCES 1
+#define NUM_ENVIRONMENTS 1
+
+ALfloat listenerPos[]={0.0,0.0,4.0};
+ALfloat listenerVel[]={0.0,0.0,0.0};
+ALfloat listenerOri[]={0.0,0.0,1.0, 0.0,1.0,0.0};
+ALfloat source0Pos[]={ -2.0, 0.0, 0.0};
+ALfloat source0Vel[]={ 0.0, 0.0, 0.0};
+
+
+ALuint  buffer[NUM_BUFFERS];
+ALuint  source[NUM_SOURCES];
+ALuint  environment[NUM_ENVIRONMENTS];
+
+ALsizei size,freq;
+ALenum  format;
+ALvoid  *data;
+
+
 
 void renderScene(void) {
 
@@ -22,6 +47,55 @@ void main(int argc, char **argv) {
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(800,600);
 	glutCreateWindow("Dea's awesome game");
+
+	//initialise openAL
+	alutInit(&argc, argv) ; 
+
+	alListenerfv(AL_POSITION,listenerPos);
+	alListenerfv(AL_VELOCITY,listenerVel);
+	alListenerfv(AL_ORIENTATION,listenerOri);
+
+	alGetError(); // clear any error messages
+
+	// Generate buffers, or else no sound will happen!
+	alGenBuffers(NUM_BUFFERS, buffer);
+
+	if(alGetError() != AL_NO_ERROR) 
+	{
+		printf("- Error creating buffers !!\n");
+		exit(1);
+	}
+	else
+	{
+		printf("init() - No errors yet.");
+	}
+
+	ALboolean loop = AL_TRUE;
+
+	alutLoadWAVFile("audio/PartyEscort.wav",&format,&data,&size,&freq,&loop);
+	alBufferData(buffer[0],format,data,size,freq);
+	alutUnloadWAV(format,data,size,freq);
+
+	alGetError(); /* clear error */
+	alGenSources(NUM_SOURCES, source);
+
+	if(alGetError() != AL_NO_ERROR) 
+	{
+		printf("- Error creating sources !!\n");
+		exit(2);
+	}
+	else
+	{
+		printf("init - no errors after alGenSources\n");
+	}
+	alSourcef(source[0], AL_PITCH, 1.0f);
+	alSourcef(source[0], AL_GAIN, 1.0f);
+	alSourcefv(source[0], AL_POSITION, source0Pos);
+	alSourcefv(source[0], AL_VELOCITY, source0Vel);
+	alSourcei(source[0], AL_BUFFER,buffer[0]);
+	alSourcei(source[0], AL_LOOPING, AL_TRUE);
+
+	alSourcePlay(source[0]);
 
 	// register callbacks
 	glutDisplayFunc(renderScene);
